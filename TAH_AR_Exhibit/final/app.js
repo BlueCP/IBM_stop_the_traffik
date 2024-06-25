1195aaf55102635eb2912c325a3918da71a4ead6
Kishok-S-123 <164889525+Kishok-S-123@users.noreply.github.com>
2024-06-25 14:21:34 +0100

    meow meow

--- TAH_AR_Exhibit/final/app.js
+++ TAH_AR_Exhibit/final/app.js
@@ -35,6 +35,8 @@ class App {
   constructor() {
     this.glbModel = null;
     this.mixer = null;
+    this.count = 0;
+    this.objects = [window.welcome, window.heatmap, window.routemap, window.example, window.stats];
   }
 
   /**
@@ -115,22 +117,78 @@ class App {
     this.xrSession.addEventListener("select", this.onSelect);
   }
 
+  createGlobe() {
+    const globe = new ThreeGlobe()
+      .globeImageUrl('https://unpkg.com/three-globe@2.31.1/example/img/earth-night.jpg');
+
+    globe.showAtmosphere(false);
+    globe.scale.set(0.001,0.001,0.001);
+    globe.position.set(0.0, 0.5, 0.0);
+    const factor = 1;
+    globe.scale.x /= factor;
+    globe.scale.y /= factor;
+    globe.scale.z /= factor;
+    globe.globeMaterial.wireframe = true;
+    return globe;
+  }
+
+  scene_insert(model, x, y, z) {
+    if (model) {
+      const clone = model.clone();
+      const offset = new THREE.Vector3(x, y, z);
+      clone.position.copy(this.reticle.position).add(offset);
+      this.scene.add(clone);
+      console.log("model inserted");
+    } else {
+      console.log('Model is undefined');
+    }
+  }
   /**
    * Add a model when the screen is tapped.
    */
-
   onSelect = () => {
-    if (this.glbModel) {
-      const clone = this.glbModel.clone();
-      clone.position.copy(this.reticle.position);
-      this.scene.add(clone);
+    // window.bow = this.createGlobe();
+    // if (window.bow) {
+    //   console.log('material: ', window.bow.globeMaterial());
+    //   console.log('scale: ', window.bow.scale);
+    //   console.log('radius: ', window.bow.getGlobeRadius());
 
-      // Create an AnimationMixer and play the animation
-      this.mixer = new THREE.AnimationMixer(clone);
-      this.playAnimation('ArrowAction');
-    } else {
-      console.log('Model not loaded yet');
-    }
+    //   this.scene_insert(window.bow, 0.0, 1.4, -100.0);
+    // } else {
+    //   console.log('window.bow is not defined');
+    // }
+    this.scene_insert(this.objects[this.count], 0.0, 1.8, 0.0);
+    // this.scene_insert(window.welcome, 0.0, 1.4, 0.0);
+    // this.scene_insert(window.heatmap, 0.25, 1.4, 0.5);
+    // this.scene_insert(window.routemap, 0.5, 1.4, 1.0);
+    // this.scene_insert(window.example, 0.75, 1.4, 1.5);
+    // this.scene_insert(window.stats, 1.0, 1.4, 2.0);
+
+    // if (this.glbModel) {
+    //   const clone = this.glbModel.clone();
+    //   clone.position.copy(this.reticle.position);
+    //   clone.scale.set(clone.scale.x / 5, clone.scale.y / 5, clone.scale.z / 5);
+    //   this.scene.add(clone);
+
+    //   // Create an AnimationMixer and play the animation
+    //   this.mixer = new THREE.AnimationMixer(clone);
+    //   this.playAnimation('ArrowAction');
+    // } else {
+    //   console.log('Model not loaded yet');
+    // }
+
+    this.scene.traverse(function (node) {
+      console.log('Name: ', node.name, ' Type: ', node.type);
+    });
+
+    // Check if the globe's material is transparent
+    // if (window.bow) {
+    //   console.log(window.bow.showAtmosphere()); // Should be false
+    // }
+
+    // Check the scene background
+    // console.log(this.scene.background); // Should be null for transparency
+    this.count += 1;
   }
 
   /**
@@ -147,66 +205,6 @@ class App {
       }
     }
   }
-
-  /** Place a sunflower when the screen is tapped. */
-  // onSelect = () => {
-  //   if (window.sunflower) {
-  //     const clone = window.sunflower.clone();
-  //     clone.position.copy(this.reticle.position);
-  //     this.scene.add(clone)
-
-  //     const shadowMesh = this.scene.children.find(c => c.name === 'shadowMesh');
-  //     shadowMesh.position.y = clone.position.y;
-  //   }
-  // }
-
-  //scene_insert(model, x,y,z){
-  //  if(model){
-  //    const clone = model.clone();
-  //    const offset = new THREE.Vector3(x,y,z);
-  //    clone.position.copy(this.reticle.position).add(offset);
-  //    this.scene.add(clone);
-  //    console.log("model inserted");
-  //  }
-  //  else{
-  //    console.log('oink');
-  //  }
-  //}
-
-  //onSelect = () => {
-    // this.scene_insert(window.beep, 0.0, 0.0, 0.0);
-  //  this.scene_insert(window.globe,-1.0, 1.0, 0.0);
-  //  this.scene_insert(window.sphere, 1.0, 1.25, 0.0);
-  //  this.scene_insert(window.msg1, 0.0, 1.4, 0.0);
-    // this.scene_insert(window.textMesh, (-1.0, 1.0, -1.0));
-
-
-  //  window.beep.scale.set(0.1,0.1,0.1);
-  //  window.beep.position.copy(this.reticle.postion).add(new THREE.Vector3(0,0,-2));
-  //  this.scene.add(window.beep);
-
-  //  this.scene.traverse(function(node){
-  //    console.log('Name: ', node.name, ' Type: ', node.type);
-  //    console.log('no relation');
-  //  });
-  //}
-
-
-
-// if(window.textMesh){
-//   const clone = window.textMesh.clone();
-//   const offset = new THREE.Vector3(-1.0, 1.0, -1.0);
-//   clone.position.copy(this.reticle.position).add(offset);
-//   this.scene.add(clone);
-//   console.log('Text added at position:', clone.position);
-// }
-// else{
-//   console.log('oink');
-// }
-
-
-
-
 
   /**
    * Called on the XRSession's requestAnimationFrame.
@@ -253,13 +251,6 @@ class App {
         this.reticle.updateMatrixWorld(true);
       }
 
-      //this.scene.traverse(function (child){
-      //  if(child.name == 'Cube'){
-      //    child.rotation.y += 0.01;
-      //  }
-
-      //});
-
       // Update the animation mixer if it exists
       if (this.mixer) {
         const delta = this.clock.getDelta();
@@ -301,7 +292,9 @@ class App {
 
     // Initialise clock for animation updates
     this.clock = new THREE.Clock();
+
   }
+
 };
 
 window.app = new App();
